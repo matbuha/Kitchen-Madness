79a949df0d74c18ec671364da6a998bd96cf33a2
ariel <95013498+matbuha@users.noreply.github.com>
2023-12-08 14:56:11 +0200

    updating to shorter scripts

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -43,4 +43,13 @@ public class KitchenObject : MonoBehaviour {
     }
 
 
+    public static KitchenObject SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+            KitchenObject kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            
+            kitchenObject.SetKitchenObjectParent(kitchenObjectParent);
+
+            return kitchenObject;
+    }
+
 }
