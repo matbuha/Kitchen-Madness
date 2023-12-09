f1696c7c5b6427e0d38c7523738d32d5ef1081bd
ariel <95013498+matbuha@users.noreply.github.com>
2023-12-09 22:32:38 +0200

    Adding plate interactions

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -122,6 +122,21 @@ public class StoveCounter : BaseCounter, IHasProgress {
             // There is KitchenObject here
             if (player.HasKitchenObject()) {
                 // Player is carrying something
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                    // Player is holding a plate
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO())) {
+                        GetKitchenObject().DestroySelf();
+                        state = State.Idle;
+
+                        OnStateChanged?.Invoke(this, new OnStateChangedEventArgs {
+                            state = state
+                        });
+
+                        OnProgressChanged?.Invoke(this, new IHasProgress.OnProgressChangedEventArgs {
+                            progressNormalized = 0f
+                        });
+                    }
+                }
             } else {
                 // Player is not carrying something
                 GetKitchenObject().SetKitchenObjectParent(player);
